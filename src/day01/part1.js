ad712ceaa3c6b449add9ed3b2a541315b9163cf8
mia.ramos <mia.ramos@alpha-research.de>
2022-12-02 01:30:53 -0300

    day part 1 done

--- src/day01/part1.js
+++ src/day01/part1.js
@@ -1,5 +1,13 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input) {
-  return 0;
+
+  const elvesByCalories = input.split("\n\n");
+  
+  const elvesByFood = elvesByCalories.map((elf)=>{
+    return elf.split("\n").reduce((food, calories)=> food + Number(calories),0);
+  });
+
+  return Math.max(...elvesByFood);
+
 }
