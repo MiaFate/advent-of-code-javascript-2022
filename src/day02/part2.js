b3a011541c6843985414906d8ee659757f96462a
mia.ramos <mia.ramos@alpha-research.de>
2022-12-02 18:37:55 -0300

    day 02 finished

--- src/day02/part2.js
+++ src/day02/part2.js
@@ -1,5 +1,35 @@
 // Advent of Code - Day 2 - Part Two
 
 export function part2(input) {
-return 0;
+/*
+* A, ROCK 1 point
+* B, PAPER 2 points
+* C, SCISSOR 3 points
+* X, LOSE = 0 points
+* Y, DRAW = 3 points
+* Z, WIN = 6 points
+*/
+
+ const scoreTable = {
+    'A X': 3,
+    'A Y': 4,
+    'A Z': 8,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 2,
+    'C Y': 6,
+    'C Z': 7,
+  };
+
+  const games = input.trim().split("\n")
+  
+  let totalScore = 0;
+
+  games.forEach(game => {
+    totalScore=totalScore+scoreTable[game]
+  });
+
+
+  return totalScore;
 }
